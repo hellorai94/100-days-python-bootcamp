89ca782776a423078c1afc170340e4479177452d
Raissa Carneiro <uefsraissa@gmail.com>
2023-11-04 19:36:35 -0300

    Fazendo uma melhoria no código do projeto 3

--- day_three/project_3.py
+++ day_three/project_3.py
@@ -7,20 +7,18 @@ print('''
 print("Bem-vindo a sua jukebox virtual")
 print("A sua missão é escolher uma música.")
 
-first = input('Pabllo Vittar ou Urias? - Insira "P" ou "U"')
-first_lower = first.lower()
+first = input('Pabllo Vittar ou Urias? - Insira "P" ou "U"').lower()
 
-if first_lower == "p": 
-    second = input('Você tá triste ou alegre? - Insira "T" ou "A"')
-    second_lower = second.lower()
-    if second_lower == "t":
+
+if first == "p": 
+    second = input('Você tá triste ou alegre? - Insira "T" ou "A"').lower()
+    if second == "t":
         print("Ouça - Triste com T")
     else: 
         print("Ouça - Zap Zum")
-elif first_lower == "u":
-    three = input('Urias cantando inglês ou português - Insira "I" ou "P"')
-    three_lower = three.lower()
-    if three_lower == "p":
+elif first == "u":
+    three = input('Urias cantando inglês ou português - Insira "I" ou "P"').lower()
+    if three == "p":
         print("Ouça - Foi Mal")
     else:
         print("Ouça - Her Mind")

6c41d7b70db4118b1b14d4ff3bef840f325a552d
Raissa Carneiro <uefsraissa@gmail.com>
2024-01-02 21:04:44 -0300

    Continuando o código do dia 12

--- day_12/project_12.py
+++ day_12/project_12.py
@@ -5,8 +5,10 @@ print("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard':").lower()
 if difficulty == 'easy':
     count = 10
+    print(f"You have {count} attempts remaining to guess the number.")
 else:
     count = 5
+    print(f"You have {count} attempts remaining to guess the number.")
 
 # 2 Make a randonic number
 number = random.randrange(1, 101)
@@ -16,23 +18,16 @@ winner = False
 
 while not winner:
     guess = int(input("Make a guess:"))
-    if difficulty == 'easy':
-        print(f"You have {count} attempts remaining to guess the number.")
+    if count == 1:
+        winner = True
+        print("You lost")
+    if count != 1:
         count -= 1
-        if count == 10:
-            print("You lost")
-            break
-    else:
         print(f"You have {count} attempts remaining to guess the number.")
-        count -= 1
-        if count == 5:
-            print("You lost")
-            break
         if guess < number:
             print("To low.\nGuess again.")
         elif guess > number:
             print("To high.\nGuess again.")
         else:
             print("You're right!")
-            break
-        
+
